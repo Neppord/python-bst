376603b398bf61d2899b287c50f75a7478baa205
Per Magnusson <killingfloor00@gmail.com>
2014-08-18 14:34:05 +0200

    Node class refactor

--- bst/node.py
+++ bst/node.py
@@ -13,29 +13,14 @@ class Node:
         return self._left_child
 
 
-    @left_child.setter
-    def left_child(self, value):
-        self._left_child = value
-
-
     @property
     def right_child(self):
         return self._right_child
 
 
-    @right_child.setter
-    def right_child(self, value):
-        self._right_child = value
-
-
     @property
     def val(self):
         return self._val
-
-
-    @val.setter
-    def val(self, value):
-        self._val = value
     
 
     def insert(self, node, root):
@@ -47,30 +32,30 @@ class Node:
 
         elif node.val < root.val:
             # insert left child
-            if not root.left_child:
-                root.left_child = node
+            if not root._left_child:
+                root._left_child = node
                 node.parent = root
             else:
-                self.insert(node, root.left_child)
+                self.insert(node, root._left_child)
 
         else:
             # insert right child
-            if not root.right_child:
-                root.right_child = node
+            if not root._right_child:
+                root._right_child = node
                 node.parent = root
             else:
-                self.insert(node, root.right_child)
+                self.insert(node, root._right_child)
 
 
     def delete(self, node):
         # if node is leaf
-        if not node.left_child and not node.right_child:
-            if node.parent.left_child is node:
-                node.parent.left_child = None
+        if not node._left_child and not node._right_child:
+            if node.parent._left_child is node:
+                node.parent._left_child = None
                 del node
                 return True
-            elif node.parent.right_child is node:
-                node.parent.right_child = None
+            elif node.parent._right_child is node:
+                node.parent._right_child = None
                 del node
                 return True
         else:
